e7ec4b80fc8741f48c09926c74c044dcf09a5503
svillar <villar.mucasan@gmail.com>
2024-03-30 23:49:34 -0300

    add before anda after all tags

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@BeforeAll
+	static void contextLoads() {
+		System.out.println("Test if test starts! ");
+	}
+	@AfterAll
+	static void afterAllTests(){
+		System.out.println("Test sessions have eneded!");
 	}
 
 }
