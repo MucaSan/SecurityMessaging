b393dbf5e0bf58a77c3154f3a3e7feaf3f8c4c27
svillar <villar.mucasan@gmail.com>
2024-04-14 13:17:48 -0300

    add spring web project

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,20 +1,13 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@BeforeAll
-	static void contextLoads() {
-		System.out.println("Test if test starts! ");
-	}
-	@AfterAll
-	static void afterAllTests(){
-		System.out.println("Test sessions have eneded!");
+	@Test
+	void contextLoads() {
 	}
 
 }
