fc787916eb73b7a91b29cfce377361cfc51f1b9e
svillar <villar.mucasan@gmail.com>
2024-04-16 00:29:45 -0300

    add DELETE and POST verbs

--- src/main/java/com/example/demo/repository/UsersController.java
+++ src/main/java/com/example/demo/repository/UsersController.java
@@ -1,17 +1,25 @@
 package com.example.demo.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("users")
 public class UsersController {
     @Autowired
     private UsersRepository repository;
-    @GetMapping("/users")
+    @GetMapping()
     public List<User> getUsers(){
             return repository.listAll();
+    }
+    @DeleteMapping("/{username}")
+    public void deleteUsers(@PathVariable("username") String username){
+        System.out.println("DELETED user-> "+username +  "SUCESSFULLY!" );
+    }
+    @PostMapping
+    public void postUser(@RequestBody User user){
+        System.out.println("User has been created!");
     }
 }
