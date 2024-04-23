de9ca12ecb888e970dab78dcf506c69aac1cee56
svillar <villar.mucasan@gmail.com>
2024-04-22 23:47:56 -0300

    add components and folders to frontend

---
+++ demoFrontEnd/src/app/crypto-app/crypto-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './crypto-app.component.html',
+  styleUrl: './crypto-app.component.scss'
+})
+export class CryptoAppComponent {
+
+}
---
+++ demoFrontEnd/src/app/crypto-app/crypto-app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CryptoAppRoutingModule } from './crypto-app-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CryptoAppRoutingModule
+  ]
+})
+export class CryptoAppModule { }
