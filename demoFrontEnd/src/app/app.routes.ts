228167ed191bb139db638eb15f620d23c8fc4d87
svillar <villar.mucasan@gmail.com>
2024-04-23 00:49:24 -0300

    add test, with display message

--- demoFrontEnd/src/app/app.routes.ts
+++ demoFrontEnd/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { LoginModule } from './login/login.module';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+import { CryptoAppComponent } from './crypto-app/crypto-app.component';
 
-export const routes: Routes = [];
+// export const routes: Routes =[
+//     {path: 'login', 
+//      component: LoginComponent,
+//     },
+//     {
+//         path: 'signin',
+//         component: SigninComponent,
+//     },
+//     {
+//         path: 'crypto-app',
+//         component: CryptoAppComponent,
+//     },
+// ];
+
+export const routes: Routes = [
+    {
+        path: '', 
+        loadChildren: ()=> 
+            import('./login/login.module').then((m)=> m.LoginModule),
+    },
+];
