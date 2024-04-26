d39856434647325ba5467a50f6a5196bba432e07
svillar <villar.mucasan@gmail.com>
2024-04-25 23:43:02 -0300

    add database connection and import dependencies

---
+++ src/main/java/controller/MessageController.java
@@ -0,0 +1,8 @@
+package controller;
+
+import jakarta.persistence.Entity;
+import org.springframework.web.bind.annotation.RestController;
+@Entity
+@RestController
+public class MessageController {
+}
