2fcf536a07b8a3f170872a4c70b5572bb9d89ed8
svillar <villar.mucasan@gmail.com>
2024-05-21 23:15:10 -0300

    add java project

---
+++ SecurityMessaging/src/main/java/com/security/messaging/SecurityMessaging/SecurityMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.security.messaging.SecurityMessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityMessagingApplication.class, args);
+	}
+
+}
---
+++ SecurityMessaging/src/test/java/com/security/messaging/SecurityMessaging/SecurityMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.messaging.SecurityMessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
